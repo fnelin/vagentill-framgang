00c46830b194ecdc5e3d13c46e13189a8fefb18e
Fredrik.N <fredrik.nelin@outlook.com>
2026-03-19 09:33:18 +0100

    Cleaned up repo
    Cleaned up and removed files/folders not needed for the project.

--- components/ui/footer.tsx
+++
@@ -1,6 +0,0 @@
-export default function SidFot() {
-    return <footer className="bg-stone-900">
-        <p className="py-6 text-center text-gray-100">© 2026 Vägen till framgång</p>
-    </footer>
-
-}
